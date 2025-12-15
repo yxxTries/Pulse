e08cf67737ec2b09797e5115be7f1595f340d28d
yxxTries <amil.shahul777@gmail.com#>
2025-12-14 20:49:32 -0400

    changes to git ignore and ui text.

--- OverlayDemo/obj/Debug/net10.0-windows/MainWindow.g.cs
+++ OverlayDemo/obj/Debug/net10.0-windows/MainWindow.g.cs
@@ -51,7 +51,7 @@ namespace OverlayDemo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/OverlayDemo;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/OverlayDemo;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
