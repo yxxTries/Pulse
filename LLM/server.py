04f6b8982387cf0cc54118bdf348ceec744f2787
yxxTries <amil.shahul777@gmail.com#>
2025-12-14 21:44:20 -0400

    Installed SGL server

---
+++ LLM/server.py
@@ -0,0 +1,14 @@
+from sglang.test.doc_patch import launch_server_cmd
+from sglang.utils import wait_for_server, print_highlight, terminate_process
+
+# This is equivalent to running the following command in your terminal
+# python3 -m sglang.launch_server --model-path qwen/qwen2.5-0.5b-instruct --host 0.0.0.0
+
+server_process, port = launch_server_cmd(
+    """
+python3 -m sglang.launch_server --model-path qwen/qwen2.5-0.5b-instruct \
+ --host 0.0.0.0 --log-level warning
+"""
+)
+
+wait_for_server(f"http://localhost:{port}")
